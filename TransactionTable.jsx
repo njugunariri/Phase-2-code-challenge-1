6eb5a7fadd9d6bd9f2f8cfc08de8d969520c6c76
njugunariri <162272898+njugunariri@users.noreply.github.com>
2024-04-29 14:15:23 +0300

    Add files via upload

---
+++ TransactionTable.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Table({ transactions }) {
+  return (
+    <div>
+      <table className="my-table">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((transaction, index) => (
+            <tr key={index}>
+              <td>{transaction.date}</td>
+              <td>{transaction.description}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.amount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Table;
